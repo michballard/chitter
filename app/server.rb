e8580fc7448ceacd46de339935f1c7e076b18471
michballard <michballard@gmail.com>
2014-09-21 08:53:21 +0100

    Directory changes

--- app/server.rb
+++ app/server.rb
@@ -16,3 +16,6 @@ enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
 set :partial_template_engine, :erb
+
+set :root, File.dirname(__FILE__)
+set :public_folder, Proc.new { File.join(root, "..", "public") }
