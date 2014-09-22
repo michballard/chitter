b75530f60c19560f66be84db374ac4fec2856944
michballard <michballard@gmail.com>
2014-09-22 23:12:05 +0100

    Message posting working properly

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -1,5 +1,9 @@
 post '/peeps' do 
 	message = params["message"]
-	Peep.create(:message => message)
+	time = Time.now
+	username = current_user.username
+	Peep.create(:message => message, 
+							:time_created => time,
+							:username => username)
 	redirect to('/')
 end
